ad41504837badd34486b32537553a6ee0eed42aa
Radha <malepatiradha@gmail.com>
2024-06-26 16:52:29 -0400

    new project

---
+++ demo-social-media/src/main/java/com/social/media/blog/app/demo/social/media/DemoSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.social.media.blog.app.demo.social.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSocialMediaApplication.class, args);
+	}
+
+}
---
+++ demo-social-media/src/test/java/com/social/media/blog/app/demo/social/media/DemoSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.media.blog.app.demo.social.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
